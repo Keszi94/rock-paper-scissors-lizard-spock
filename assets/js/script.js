3fe2ea63565178e68e25ebc87b7a4edd14aab457
Eszter Kiskarpati <keszi94@gmail.com>
2024-07-31 15:58:32 +0000

    Add constants for DOM elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,10 @@
 /**
  * Declare constants for DOM elements
  */
-const buttons = ;
-const playerScore = ;
-const computerScore = ;
-const playerImage = ;
-const computerImage = ;
-const message = ;
+const buttons = document.getElementsByClassName("control");
+const playerScore = document.getElementById("player-score");
+const computerScore = document.getElementById("computer-score");
+const playerImage = document.getElementById("player-image");
+const computerImage = document.getElementById("computer-image");
+const message = document.getElementById("message");
 const choices = ["Rock", "Paper", "Scissors", "Lizard", "Spock"];
