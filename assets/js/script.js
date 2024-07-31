4af069087006a246cf2e8471bb7da0d6392a3efc
Eszter Kiskarpati <keszi94@gmail.com>
2024-07-31 15:55:35 +0000

    Add basic htmls structure for heading and game area, add global styles in style.css

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+/**
+ * Declare constants for DOM elements
+ */
+const buttons = ;
+const playerScore = ;
+const computerScore = ;
+const playerImage = ;
+const computerImage = ;
+const message = ;
+const choices = ["Rock", "Paper", "Scissors", "Lizard", "Spock"];
