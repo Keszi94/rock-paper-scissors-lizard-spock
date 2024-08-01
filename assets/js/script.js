14bba24e9b594f9c9e26ea602c4577144084ec15
Eszter Kiskarpati <keszi94@gmail.com>
2024-08-01 16:16:49 +0000

    Add the runGame function, set the computerChoice to be random by using math.random. Make the function display images based on the player's and the computer's choice in the game area

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,28 @@ const playerImage = document.getElementById("player-image");
 const computerImage = document.getElementById("computer-image");
 const message = document.getElementById("message");
 const choices = ["Rock", "Paper", "Scissors", "Lizard", "Spock"];
+
+/**
+ * Event listeners
+ */
+for (let button of buttons) {
+    button.addEventListener("click", function () {
+        let playerChoice = this.getAttribute("data-choice");
+        runGame(playerChoice);
+    })
+}
+
+
+/**
+ * All functions
+ */
+function runGame(playerChoice) {
+
+    playerImage.src = `assets/images/${choices[playerChoice]}.png`;
+    playerImage.alt = choices[playerChoice];
+
+    let computerChoice = Math.floor(Math.random() * 5);
+
+    computerImage.src = `assets/images/${choices[computerChoice]}.png`;
+    computerImage.alt = choices[computerChoice];
+}
