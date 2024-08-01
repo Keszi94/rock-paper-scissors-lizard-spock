690e9af04e6eac36a050b64d130c57e586553835
Eszter Kiskarpati <keszi94@gmail.com>
2024-08-01 17:20:38 +0000

    Add checkWinner function to get the winner of each match. Add the messages text content to display the winner on the screen

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,6 +23,8 @@ for (let button of buttons) {
 /**
  * All functions
  */
+
+// The main game function - checks the data when using the playerChoice button
 function runGame(playerChoice) {
 
     playerImage.src = `assets/images/${choices[playerChoice]}.png`;
@@ -32,4 +34,30 @@ function runGame(playerChoice) {
 
     computerImage.src = `assets/images/${choices[computerChoice]}.png`;
     computerImage.alt = choices[computerChoice];
+
+    let matchResult = checkWinner(choices[computerChoice], choices[playerChoice]);
+
+    updateScores(matchResult);
 }
+
+// Checks who the winner is
+function checkWinner(playerChoice, computerChoice) {
+
+    if (playerChoice === computerChoice) {
+        message.textContent = "It's a tie!";
+        return "tie"
+    } else if (
+        (playerChoice === "Rock" && (computerChoice === "Scissors" || "Lizard")) ||
+        (playerChoice === "Paper" && (computerChoice === "Rock" || computerChoice === "Spock")) ||
+        (playerChoice === "Scissors" && (computerChoice === "Paper" || computerChoice === "Lizard")) ||
+        (playerChoice === "Lizard" && (computerChoice === "Paper" || computerChoice === "Spock")) ||
+        (playerChoice === "Spock" && (computerChoice === "Rock" || computerChoice === "Scissors"))
+    ) {
+        message.textContent = "You win this round!"
+        return "player";
+    } else {
+        message.textContent = "The computer wins this round!";
+        return "computer";
+    }
+}
+
