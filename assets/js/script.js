7c1c7344830b44406be7eb309fddc04e51d239c2
Eszter Kiskarpati <keszi94@gmail.com>
2024-08-01 21:09:44 +0000

    Add a resetGame function to javascript to restart the game after a winner is declared. Give new css classes to the winner messages in javascript to change their color according to results

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,7 +8,7 @@ const playerImage = document.getElementById("player-image");
 const computerImage = document.getElementById("computer-image");
 const message = document.getElementById("message");
 const choices = ["Rock", "Paper", "Scissors", "Lizard", "Spock"];
-const rounds = document.getElementsByClassName("rounds");
+const rounds = document.getElementsByClassName("round");
 
 let maxRounds = 0;
 let currentRound = 0;
@@ -30,8 +30,12 @@ for (let button of buttons) {
 // Event listener for the buttons on how many rounds the player wants to play
 for (let round of rounds) {
     round.addEventListener("click", function () {
-        let roundChoice = this.getAttribute("data-choice");
-        matchCount(roundChoice);
+        maxRounds = parseInt(this.getAttribute("data-choice"));
+        currentRound = 0;
+        resetGame();
+        message.textContent = `You set the game to ${maxRounds} rounds. Make your first move!`
+        // Remove the colored letters classes from this text
+        message.classList.remove("orangeText", "greenText", "redText");
     })
 }
 
@@ -51,6 +55,11 @@ function runGame(playerChoice) {
     computerImage.alt = choices[computerChoice];
 
     let matchResult = checkWinner(choices[computerChoice], choices[playerChoice]);
+
+    currentRound++;
+    if (currentRound >= maxRounds) {
+        declareWinner();
+    }
 
     updateScores(matchResult);
 }
@@ -86,6 +95,35 @@ function updateScores(matchResult) {
     }
 }
 
-function matchCount(
+// Declares the winner after the chosen amount of turns are finished
+function declareWinner() {
+    let playerFinalScore = parseInt(playerScore.textContent);
+    let computerFinalScore = parseInt(computerScore.textContent);
 
-)
+    if (playerFinalScore > computerFinalScore) {
+        message.textContent = "Congrats! You are the winner! Start a new game!"
+        // give the text color by adding a class to it from style.css
+        message.classList.add("greenText");
+        // remove all other possible classes
+        message.classList.remove("redText", "orangeText");
+        } else if (playerFinalScore < computerFinalScore) {
+        message.textContent = "Awww! You lost! Better luck in the next game!"
+        message.classList.add("redText");
+        message.classList.remove("orangeText", "greenText");
+    } else {
+        message.textContent = "It's a tie! Start a new game!"
+        message.classList.add("orangeText");
+        message.classList.remove("greenText", "redText");
+    }
+    maxRounds = 0;
+
+}
+
+// Resets the game after choosing a new round 
+function resetGame() {
+    playerScore.textContent = "0";
+    computerScore.textContent = "0";
+// include images to reload them as well 
+    playerImage.src = "assets/images/rpsls.png";
+    computerImage.src = "assets/images/rpsls.png";
+}
